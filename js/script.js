47b7d652280b967a42cc5a1cda6543f0b31a03a0
Rohan Gosavi <64428362+rohangosavi01@users.noreply.github.com>
2021-09-20 09:21:25 -0500

    Add files via upload
    Initial Website Upload version 1.0
    -includes completed responsive nav bar
    -incomplete home screen (flexbox version) 
    -initial v1

---
+++ js/script.js
@@ -0,0 +1,66 @@
+const navMob = () => {
+    const a = document.querySelector('.nav_mob');
+    const b = document.querySelector('.menu');
+    const c = document.querySelectorAll('.menu li');
+
+    a.addEventListener('click', () => {
+        b.classList.toggle('nav_mob-active');
+
+    c.forEach((link, index)=>{
+        if(link.style.animation){
+            link.style.animation='';
+        }else{
+        link.style.animation=link.style.animation = `nav_mob-fade 0.5s ease forwards ${index/7 }s`;
+        }
+    });
+
+    a.classList.toggle('toggle');
+
+    });
+}
+
+navMob();
+
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Developer", "Designer", "Leader"],
+        typeSpeed: 120,
+        backSpeed: 60,
+        loop: true
+    });
+});
+
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('mouseover', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	//Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 250 * (idx+1))
+});
+
+//inspect element blocker
+//$(document).bind("contextmenu",function(e) {
+ //   e.preventDefault();
+ //  });
+ //  
+ //  $(document).keydown(function(e){
+ //      if(e.which === 123){
+  //        return false;
+  //     }
+ //  });
+
+ /*!
+ * Cursor Name.js
+ * -- 90's cursors collection
+ */
+
+ 
